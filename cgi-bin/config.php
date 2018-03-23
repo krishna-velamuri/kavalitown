5dadcf98a949847facac46cb48b99b3db24b6aea
krishna-velamuri <kittu.sra@gmail.com>
2018-03-23 21:21:48 +0530

    Update config.php

--- cgi-bin/config.php
+++ cgi-bin/config.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "tcp:kavali.database.windows.net,1433";
+$servername = "kavali.database.windows.net";
 $username = "anupama.krishna";
 $password = "kavali@1db";
 $dbname = "kavalinews";
