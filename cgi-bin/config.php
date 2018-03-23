f99d62f791b477049538c92d40778ad86412f424
krishna-velamuri <kittu.sra@gmail.com>
2018-03-23 20:28:54 +0530

    Update config.php
    connection string changed as per azure db

--- cgi-bin/config.php
+++ cgi-bin/config.php
@@ -1,9 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "KavaliNews";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$connectionInfo = array("UID" => "anupama.krishna@kavali", "pwd" => "kavali@1db", "Database" => "kavalinews", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:kavali.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
