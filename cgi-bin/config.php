aec303adc1eeda52deb2e65e6b995acfc1b02fc8
krishna-velamuri <kittu.sra@gmail.com>
2018-03-23 21:21:04 +0530

    Update config.php

--- cgi-bin/config.php
+++ cgi-bin/config.php
@@ -1,10 +1,10 @@
 <?php
-try {
-    $conn = new PDO("sqlsrv:server = tcp:kavali.database.windows.net,1433; Database = kavalinews", "anupama.krishna", "kavali@1db");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+$servername = "tcp:kavali.database.windows.net,1433";
+$username = "anupama.krishna";
+$password = "kavali@1db";
+$dbname = "kavalinews";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
 }
 ?>
